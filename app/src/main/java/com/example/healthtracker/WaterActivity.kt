4b74e767683569a6b3024349b956fbefb911c55f
Hugo Jordan <hugoj8366@gmail.com>
2023-12-28 14:54:40 -0300

    Adicionando quantidade de exercícios em WaterActivity

--- app/src/main/java/com/example/healthtracker/WaterActivity.kt
+++ app/src/main/java/com/example/healthtracker/WaterActivity.kt
@@ -2,9 +2,7 @@ package com.example.healthtracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import android.widget.*
 import androidx.annotation.StringDef
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AlertDialog
@@ -13,6 +11,7 @@ private lateinit var editWeight: EditText
 private lateinit var editAge: EditText
 private lateinit var button: Button
 private lateinit var editQuantity: EditText
+private lateinit var autoExercise: AutoCompleteTextView
 
 class WaterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +21,13 @@ class WaterActivity : AppCompatActivity() {
         editAge = findViewById(R.id.water_age)
         editQuantity = findViewById(R.id.water_quantity)
         button = findViewById(R.id.water_button)
+
+        autoExercise = findViewById(R.id.auto_exercise)
+        val arrayExercise: Array<String> = resources.getStringArray(R.array.exercise_frequency)
+        autoExercise.setText(arrayExercise.first())
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,arrayExercise)
+        autoExercise.setAdapter(adapter)
+
         button.setOnClickListener {
             if(!validate()){
                 Toast.makeText(this,R.string.toast_invalid_info,Toast.LENGTH_SHORT).show()
@@ -30,8 +36,8 @@ class WaterActivity : AppCompatActivity() {
             val weight = editWeight.text.toString().toInt()
             val age = editAge.text.toString().toInt()
             val quantity = editQuantity.text.toString().toDouble()
-            val idealQuantityWater = calculateIdealQuantityWater(age,weight)
-            val idealQuantityWaterMl = (idealQuantityWater*1000.0).toInt()
+            val idealQuantityWater = calculateIdealQuantityWater(age,weight) + quantityByExercise(arrayExercise)
+            val idealQuantityWaterL: Double = idealQuantityWater/1000.0
 
             @StringRes
             val titleType: Int =
@@ -40,7 +46,7 @@ class WaterActivity : AppCompatActivity() {
 
             AlertDialog.Builder(this).apply {
                 setTitle(titleType)
-                setMessage(getString(R.string.dialog_water_message,idealQuantityWaterMl,idealQuantityWater))
+                setMessage(getString(R.string.dialog_water_message,idealQuantityWater,idealQuantityWaterL))
                 setPositiveButton(R.string.ok){_,_->}
                 create()
                 show()
@@ -55,12 +61,23 @@ class WaterActivity : AppCompatActivity() {
                 !editAge.text.toString().startsWith("0"))
     }
 
-    private fun calculateIdealQuantityWater(age: Int, weight: Int): Double{
+    private fun calculateIdealQuantityWater(age: Int, weight: Int): Int{
         return when(age){
-            in 0 .. 17 -> (40 * weight)/1000.0
-            in 18 .. 55 -> (35 * weight)/1000.0
-            in 56 .. 65 -> (30 * weight)/1000.0
-            else -> (25 * weight)/1000.0
+            in 0 .. 17 -> (40 * weight)
+            in 18 .. 55 -> (35 * weight)
+            in 56 .. 65 -> (30 * weight)
+            else -> (25 * weight)
+        }
+    }
+
+    private fun quantityByExercise(array: Array<String>): Int{
+        return when(autoExercise.text.toString()){
+            array[0] -> 0
+            array[1] -> 325
+            array[2] -> 750
+            array[3] -> 1075
+            array[4] -> 1500
+            else -> 2250
         }
     }
 }
