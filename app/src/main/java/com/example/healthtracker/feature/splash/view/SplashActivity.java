e3aa471b54c778b5730f436c980d096d7d4153a1
Hugo Jordan <hugoj8366@gmail.com>
2025-01-20 23:22:12 -0300

    refactor: change splash to java

---
+++ app/src/main/java/com/example/healthtracker/feature/splash/view/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.healthtracker.feature.splash.view;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.healthtracker.R;
+import com.example.healthtracker.feature.main.view.MainActivity;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1500);
+    }
+}
